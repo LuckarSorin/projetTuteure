0edaf055df2e39718ab6b13a6bf6b0303e669323
Colisalalia-FR <121874309+Colisalalia-FR@users.noreply.github.com>
2023-03-12 13:56:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    /*si connecté renvoie à home.php*/
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>eHo - Accueil</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+        <link rel="icon" type="image/svg" href="img/favicon.svg">
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>-->
+    </head>
+    <body class="fond_sombre">
+        <!--<div id="misesajour"></div>
+        <h1>eHo</h1>
+        <button><a>Jouer</a></button>
+        <div>
+            <button><a href="./seconnecter.php">Connexion</a></button>
+            <button><a href="./creationcompte.php">Créer un compte</a></button>
+        </div>
+        <div id="infos"></div>-->
+        <div class="accueil_centre">
+            <div class="flex_colonne">
+                <div class="flex bandeau_superieur">
+                    <h3>eHo</h3>
+                    <img src="img/placeholder.png" alt="Logo de l'application eHo">
+                </div>
+                <div>
+                    <h2><a href="">JOUER</a></h2>
+                </div>
+                <div>
+                    <div><p><a href="seconnecter.php">Connexion</a></p></div>
+                    <p><a href="creationcompte.php">Créer un compte</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

f7781542526e271d43717f5f0fa8dfdf608b09c1
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-03-13 09:12:31 +0100

    grille du formulaire

--- creationcompte.php
+++ creationcompte.php
@@ -8,16 +8,34 @@
     <body>
         <h1>eHo</h1>
         <form action="./script/inscription.php" method="POST">
-            <label>Nom de compte</label>
-            <input type="text" name="username" required/>
-            <label>Adresse Mail</label>
-            <input type="email" name="mail" required/>
-            <label>Mot de passe</label>
-            <input type="password" name="mot de passe" required/>
-            <label>Répéter mot de passe</label>
-            <input type="password" name="mot de passe" required/>
-            
-            <input type="submit" name="enter" value="S'inscrire">
+            <tr>
+                <td>
+                    <label>Nom de compte</label>
+                    <input type="text" name="username" required/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Adresse Mail</label>
+                    <input type="email" name="mail" required/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Mot de passe</label>
+                    <input type="password" name="mot de passe" required/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Répéter mot de passe</label>
+                    <input type="password" name="mot de passe" required/>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="submit" name="enter" value="S'inscrire">
         </form>
     </body>
 </html>

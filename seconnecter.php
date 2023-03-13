a0a103825f2ad786216d6b43f7f00bb9ef0588d2
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-03-13 09:05:54 +0100

    correction form

--- seconnecter.php
+++ seconnecter.php
@@ -9,11 +9,11 @@
         <h1>eHo</h1>
         <form action="./script/connexion.php" method="POST">
             <label>Nom de compte</label>
-            <input />
+            <input type="text" name="username" required />
             <label>Mot de passe</label>
-            <input />
+            <input type="password" name="username" required />
             
-            <button><a>Connexion</a></button>
+            <input type="submit" name="enter" value="Se connecter">
         </form>
     </body>
 </html>

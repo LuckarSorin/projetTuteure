ddc4e71d5685afd2cf1415d474bd5c9e804a14a2
Colisalalia-FR <121874309+Colisalalia-FR@users.noreply.github.com>
2023-05-02 22:26:36 +0200

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,38 @@
+<?php
+    //TODO
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>eHo - Accueil</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+        <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
+    </head>
+    <body class="fond_sombre">
+        <aside class="fond_sombre"></aside>
+        <main>
+            <nav class="fond_sombre">
+                <h2 class="center">Mes sauvegardes</h2>
+            </nav>
+            <section class="configurations">
+                <div>
+                    <p>Configuration 1 : Ma maison</p>
+                    <p><span>19/01/2023 20h38</span></p>
+                </div>
+                <div>
+                    <p>Configuration 2 : Ma maison de campagne</p>
+                    <p><span>23/01/2023 12h08</span></p>
+                </div>
+                <div>
+                    <p>Configuration 3 : Mon appartement</p>
+                    <p><span>20/02/2023 00h02</span></p>
+                </div>
+                <div>
+                    <p>Configuration 4 : Ma maison 2</p>
+                    <p><span>19/03/2023 14h34</span></p>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>

4c1b5a0352a44ee68b037dd09c9d333f765f40e4
Colisalalia-FR <121874309+Colisalalia-FR@users.noreply.github.com>
2023-05-02 22:26:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,39 @@
 <?php
     /*si connecté renvoie à home.php*/
 ?>
-
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
-        <title>eHo - Accueil</title>
+        <title>eHo - Configurez, Visualisez, Économisez !</title>
         <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
-        <link rel="icon" type="image/svg" href="img/favicon.svg">
+        <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
         <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>-->
     </head>
     <body class="fond_sombre">
-        <div id="misesajour"></div>
-        
         <div class="accueil_centre">
-            <div class="flex_colonne">
+            <div class="flex flex_colonne">
                 <div class="flex bandeau_superieur">
-                    <h3>eHo</h3>
-                    <img src="img/placeholder.png" alt="Logo de l'application eHo">
+                    <img src="assets/img/placeholder.png" alt="Logo de l'application eHo">
                 </div>
                 <div class="flex hover_decoration">
                     <hr>
-                    <h2><a href="./home.php">JOUER</a></h2>
+                    <h2 class="center"><a href="./home.php">JOUER</a></h2>
                     <hr>
                 </div>
                 <div>
                     <div class="flex hover_decoration">
                         <hr>
-                        <p><a href="seconnecter.php">Connexion</a></p>
+                        <p class="center"><a href="seconnecter.php">Connexion</a></p>
                         <hr>
                     </div>
                     <div class="flex hover_decoration">
                         <hr>
-                        <p><a href="creationcompte.php">Créer un compte</a></p>
+                        <p class="center"><a href="creationcompte.php">Créer un compte</a></p>
                         <hr>
                     </div>
                 </div>
             </div>
         </div>
-        
-        <div id="infos"></div>
     </body>
 </html>

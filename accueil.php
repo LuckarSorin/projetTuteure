290c9d60df7c98dc7f61a7600adcd571d125ac67
Colisalalia-FR <121874309+Colisalalia-FR@users.noreply.github.com>
2023-05-20 12:06:32 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -8,17 +8,18 @@
         <title>eHo - Accueil</title>
         <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
         <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
+        <script src="assets/js/jquery.js"></script>
     </head>
     <body class="fond_sombre flex">
         <aside class="fond_sombre flex menu">
             <figure class="eho">
-                <a href="https://eho.esteam.mmi-velizy.fr">
+                <a href="https://www.eho.esteam.mmi-velizy.fr">
                     <img src="assets/img/logo_bottom.svg" alt="eHo" title="eHo">
                 </a>
             </figure>
             <details>
                 <summary>Nouvelle simulation</summary>
-                <p><a href="https://eho.esteam.mmi-velizy.fr/jeu.php">Nouvelle simulation</a></p>
+                <p><a href="https://www.eho.esteam.mmi-velizy.fr/jeu.php">Nouvelle simulation</a></p>
             </details>
             <details>
                 <summary>Mon compte</summary>
@@ -28,8 +29,8 @@
             </details>
             <details>
                 <summary>Mes sauvegardes</summary>
-                <p><a href="?page=1">Consulter mes sauvegardes</a></p>
-                <p><a href="?page=2">Gérer mes sauvegardes</a></p>
+                <p class="ajax_include 1">Consulter mes sauvegardes</p>
+                <p class="ajax_include 2">Gérer mes sauvegardes</p>
                 <p><a href="?page=3">Ajouter une sauvegarde</a></p>
             </details>
             <details>
@@ -41,7 +42,9 @@
         </aside>
         <main>
             <?php
-                $page = $_GET['page'];
+                /*
+                
+                $page = $_GET['page'] ?? 0;
                 
                 switch ($page) {
                     case "1":
@@ -57,9 +60,63 @@
                         include('./accueil/sauvegardes.html');
                 }
                 
+                */
+                
                 // A faire en JQuery finalement, ce sera mieux. Si possible prévoir le cas où JS désactivé
             ?>
         </main>
         <!-- Sélection des configurations en JQuery -->
+        <script>
+            $(document).ready(function() {
+                // Fonction qui appelle via AJAX le fichier ajax_accueil_includes.php qui s'occupe de [...]
+                function getInclude(number) {
+                    jQuery.ajax({
+                        type: "POST",
+                        url: "ajax/ajax_accueil_includes.php",
+                        dataType: "text",
+                        data: {
+                            getInclude : number
+                        },
+                        success: function(response) {
+                            console.log(response);
+                            $("main").load(response);
+                            /*if (JSON.parse(response) == true) {
+                                console.log("Test cookie accepté");
+                            }
+                            else {
+                                console.log("Test cookie refusé");
+                            }*/
+                        }
+                    });
+                    console.log("AJAX DONE");
+                }
+                
+                $('p.ajax_include').on('click', function(){
+                    $this = $(this);
+                    getInclude($this.attr('class').substr($this.attr('class').indexOf(" ") + 1));
+                });
+                
+                /*
+                
+                // Détection des clics des boutons de consentement des cookies
+                $('button.cookies_acceptes').on('click', function(){
+                    googleMapsCookiesAccept(true);
+                    $('aside.panneau_cookies').animate({opacity: 'hide', height: 'hide'}, 'slow');
+                });
+                $('button.cookies_refuses').on('click', function(){
+                    googleMapsCookiesAccept(false);
+                    $('aside.panneau_cookies').animate({opacity: 'hide', height: 'hide'}, 'slow');
+                });
+                // Détection du clic sur l'image pour ouvrir le panneau de configuration des cookies
+                $('img.ouvrir_cookies').on('click', function() {
+                    $('aside.panneau_cookies').animate({opacity: 'toggle', height: 'toggle'}, 'slow');
+                });
+                $('button.fermer_panneau').on('click', function() {
+                    $('aside.panneau_cookies').animate({opacity: 'hide', height: 'hide'}, 'slow');
+                });
+                
+                */
+            });
+        </script>
     </body>
 </html>

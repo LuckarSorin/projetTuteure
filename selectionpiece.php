852ecbb1a50898393084ae4d0e33247fb282df80
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2023-05-29 17:37:01 +0200

    Update selectionpiece.php

--- selectionpiece.php
+++ selectionpiece.php
@@ -8,9 +8,9 @@
 <body>
     <div id="sidebar">
         <h2>Pièces</h2>
-        <div class="menu-item"><a href="#" onclick="addRoom('Cuisine', 'cuisine.php'); return false;">Cuisine</a></div>
-        <div class="menu-item"><a href="#" onclick="addRoom('Salon', 'salon.php'); return false;">Salon</a></div>
-        <div class="menu-item"><a href="#" onclick="addRoom('Chambre', 'chambre.php'); return false;">Chambre</a></div>
+        <div class="menu-item"><a href="#" onclick="addRoom('Cuisine', 'selectionequipement.php'); return false;">Cuisine</a></div>
+        <div class="menu-item"><a href="#" onclick="addRoom('Salon', 'selectionequipement.php'); return false;">Salon</a></div>
+        <div class="menu-item"><a href="#" onclick="addRoom('Chambre', 'selectionequipement.php'); return false;">Chambre</a></div>
         <!-- Ajoutez d'autres pièces ici avec leurs liens -->
         <button onclick="toggleSidebar()">Rétracter</button>
     </div>

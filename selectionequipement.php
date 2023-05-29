87029b60eee032f50ecaa04f77b97cbdc84e4899
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2023-05-29 17:36:19 +0200

    Update and rename selectionequipement to selectionequipement.php

---
+++ selectionequipement.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Salon</title>
+     <link rel="stylesheet" type="text/css" href="stylesheet/styleMaison.css">
+     <script src="assets/js/selection.js"></script>
+</head>
+<body>
+    <div id="sidebar">
+        <h2>Équipements</h2>
+        <ul>
+            <li><button onclick="addEquipment('Télévision')">Télévision</button></li>
+            <li><button onclick="addEquipment('Lumière')">Lumière</button></li>
+ 
+            <li><button onclick="addEquipment('Lampe')">Lampe</button></li>
+
+        </ul>
+        <button onclick="toggleSidebar()">Rétracter</button>
+    </div>
+
+    <div id="content">
+        <h1>Salon</h1>
+
+        <div id="room-container" class="container"></div>
+    </div>
+</body>
+</html>

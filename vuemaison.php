6b4e44870fab87ed366aa227d2c445df525eb27d
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-05-29 17:23:23 +0200

    vuepiece

--- vuemaison.php
+++ vuemaison.php
@@ -3,11 +3,27 @@
 ?>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
         <title>eHo - Votre maison</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+        <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>-->
     </head>
+
     <body>
-        <div id="menu"></div>
-        <button><a></a></button>
-        <div id="page"></div>
+
+        <div id="menu">
+
+        </div>
+
+        <button><a>←</a></button>
+
+        <div class="fond_sombre">
+
+        </div>
+
+        <button><a>Simuler</a></button>
+
     </body>
 </html>
--- vuepiece.php
+++ vuepiece.php
@@ -3,11 +3,25 @@
 ?>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
         <title>eHo - Votre pièce</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+        <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>-->
     </head>
+
     <body>
-        <div id="menu"></div>
-        <button><a></a></button>
-        <div id="page"></div>
+
+        <div id="menu">
+
+        </div>
+
+        <button><a>←</a></button>
+
+        <div class="fond_sombre">
+
+        </div>
+
     </body>
 </html>

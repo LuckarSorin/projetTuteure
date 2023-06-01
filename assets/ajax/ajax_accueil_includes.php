fdbd5ff3da2e114898a80ec5a7cd82d57c9e3931
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-06-01 22:09:00 +0200

    correction
    je suis juste pas très fort

--- assets/ajax/ajax_accueil_includes.php
+++ assets/ajax/ajax_accueil_includes.php
@@ -2,21 +2,21 @@
     if (isset($_POST["getInclude"])) {
         switch ($_POST["getInclude"]) {
             case "1":
-                $include = "../../includes/sauvegardes.php";
+                $include = "includes/sauvegardes.php";
                 break;
             case "2":
-                $include = "../../includes/sauvegardes.php";
+                $include = "includes/sauvegardes.php";
                 break;
             case "3":
-                $include = "../../includes/sauvegardes.php";
+                $include = "includes/sauvegardes.php";
                 break;
             default:
-                $include = "../../includes/sauvegardes.php";
+                $include = "includes/sauvegardes.php";
         }
     }
     else {
-        header('Location: ../../accueil.php');
+        header('Location: accueil.php');
         exit();
     }
-    echo $include ?? "../../includes/sauvegardes.php";
+    echo $include ?? "includes/sauvegardes.php";
 ?>

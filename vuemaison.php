74191cc1006fa00cc307f1c8b0a6cab80aff91f4
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-06-05 14:06:55 +0200

    fusion pages de pièces et équipements

--- vuemaison.php
+++ vuemaison.php
@@ -7,29 +7,28 @@
         <meta name="viewport" content="width=device-width">
         <title>eHo - Ma maison</title>
         <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
-        <link rel="stylesheet" type="text/css" href="stylesheet/styleMaison.css">
         <link rel="icon" type="image/svg" href="assets/img/favicon.svg">
         <script src="assets/js/selection.js"></script>
     </head>
 
-    <body>
+    <body style="display: flex;">
 
         <div id="menu sidebar">
             <h2>Pièces</h2>
             <div class="menu-item">
-                <a href="#" onclick="addRoom('Cuisine', 'selectionequipement.php'); return false;">Cuisine</a>
+                <a href="#" onclick="addRoom('Cuisine', 'vuepiece.php'); return false;">Cuisine</a>
             </div>
             <div class="menu-item">
-                <a href="#" onclick="addRoom('Salon', 'selectionequipement.php'); return false;">Salon</a>
+                <a href="#" onclick="addRoom('Salon', 'vuepiece.php'); return false;">Salon</a>
             </div>
             <div class="menu-item">
-                <a href="#" onclick="addRoom('Chambre', 'selectionequipement.php'); return false;">Chambre</a>
+                <a href="#" onclick="addRoom('Chambre', 'vuepiece.php'); return false;">Chambre</a>
             </div>
             <!-- Ajoutez d'autres pièces ici avec leurs liens -->
-            <button onclick="toggleSidebar()">Rétracter</button>
+            <!--button onclick="toggleSidebar()">Rétracter</button-->
         </div>
 
-        <button><a> ← </a></button>
+        <!--button><a> ← </a></button-->
 
         <div class="fond_sombre">
 

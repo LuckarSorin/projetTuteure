75e297072f0871b4a0c78c67acd7862c7d370166
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2023-06-06 13:34:59 +0200

    erreur de lien

--- jeu.php
+++ jeu.php
@@ -31,7 +31,7 @@
                             include("./includes/choix_habitants.php");
                         }
                         else {
-                            include("includes/vuemaison.php");
+                            include("vuemaison.php");
                         }
                     }
                     else {
